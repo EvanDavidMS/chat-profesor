32ca5cb39f2e61b647691d15c95b0e4186255661
EvanDavidMS <105825069+EvanDavidMS@users.noreply.github.com>
2025-02-28 10:47:13 -0600

    Update app.py

--- app.py
+++ app.py
@@ -274,3 +274,6 @@ if __name__ == '__main__':
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         os.makedirs(app.config['UPLOAD_FOLDER'])
     app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5001)))
+
+
+#iniciar
